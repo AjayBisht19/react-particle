c19700e6c9a284b221f4664b7d0069ee7e096c27
AjayBisht19 <ajayb381@gmail.com>
2022-11-09 00:11:33 +0530

    form added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import * as React from 'react';
+import * as React from "react";
 import useWindowSize from "@rooks/use-window-size";
 import ParticleImage, {
   ParticleOptions,
   Vector,
   forces,
-  ParticleForce
+  ParticleForce,
 } from "react-particle-image";
-
 
 const particleOptions = {
   filter: ({ x, y, image }) => {
@@ -15,13 +14,13 @@ const particleOptions = {
     // Make a particle for this pixel if blue > 50 (range 0-255)
     return pixel.b > 50;
   },
-  color: ({ x, y, image }) => "#fff",
+  color: ({ x, y, image }) => "#0275d8",
   radius: () => Math.random() * 2.5 + 0.5,
   mass: () => 40,
   friction: () => 0.15,
   initialPosition: ({ canvasDimensions }) => {
     return new Vector(canvasDimensions.width / 2, canvasDimensions.height / 2);
-  }
+  },
 };
 
 const motionForce = (x, y) => {
@@ -33,19 +32,55 @@ function App() {
 
   return (
     <>
-     {/* <img src='/iaf.png'/> */}
-     <ParticleImage
-      src={"/iaf_white.png"}
-      width={Number(innerWidth)}
-      height={Number(innerHeight)}
-      scale={1.25}
-      entropy={20}
-      maxParticles={4000}
-      particleOptions={particleOptions}
-      mouseMoveForce={motionForce}
-      touchMoveForce={motionForce}
-      backgroundColor="#000"
-    />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexWrap:"wrap"
+        }}
+      >
+        <ParticleImage
+          src={"/iaf_white.png"}
+          width={Number(innerWidth * 0.4)}
+          height={Number(innerHeight)}
+          scale={1.25}
+          entropy={20}
+          maxParticles={4000}
+          particleOptions={particleOptions}
+          mouseMoveForce={motionForce}
+          touchMoveForce={motionForce}
+          backgroundColor="#fff"
+        />
+<div class="card p-5 shadow p-3 mb-5 bg-body rounded">
+  <div class="card-body">
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            flexWrap:"wrap"
+          }}
+        >
+          <img  style={{ marginBottom: "30px" }} src="http://www.costacloud.com/assets/img/logo1-default.png" />
+          <p>
+            Solutions Designed for Your Business eOffice for Government,
+            Procurement Automation, <br />
+          </p>
+          <p  style={{ marginBottom: "30px" }}>Health Records and may more...</p>
+
+          <h2>Sign in to your account.</h2>
+          <div style={{width:"100%"}}>
+          <input  class="form-control m-3" type="text" placeholder="Username" aria-label="default input example"/>
+          <input  class="form-control m-3" type="password" placeholder="password" aria-label="default input example"/>
+          </div>
+          <button type="button" class="btn btn-primary">submit</button>
+
+        </div>
+        </div>
+        </div>
+      </div>
     </>
   );
 }
